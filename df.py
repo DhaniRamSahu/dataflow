a7cee17dfb5f3a698606c1e9e0895c0e66230a29
Dhani Ram Sahu <dhaniramon8@gmail.com>
2024-03-17 21:50:54 +0530

    Update df.py

--- df.py
+++ df.py
@@ -18,3 +18,20 @@ def sample_list_jobs(project_id):
 #sample_list_jobs('spheric-crowbar-406014')  # Replace 'your-project-id' with your actual project ID
 if __name__ == "__main__":
         sample_list_jobs('spheric-crowbar-406014')
+
+#----
+
+from google.cloud import dataflow_v1beta3
+# from google.api_core.exceptions import GoogleAPICallError
+
+project_id = "spheric-crowbar-406014"
+region = "europe-west2"
+
+def sample_list_jobs(project_id):
+    client = dataflow_v1beta3.JobsV1Beta3Client()
+    request = client.list_jobs(request={"project_id":project_id,"location":region})
+    # print(request.jobs[0].id)
+    for i in request:
+        print(i.id)
+
+sample_list_jobs('spheric-crowbar-406014')
